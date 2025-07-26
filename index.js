39dbf64932adec0dee410051bb554dee042a6e11
naz1227 <nazishahm@gmail.com>
2025-07-27 00:14:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const fs = require('fs').promises;
+const puppeteer = require('puppeteer'); 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
